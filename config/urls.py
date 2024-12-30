74156a23a8f6d98c696e3f3cd8dc870228472319
Roy Manigley <roy.manigley@gmail.com>
2024-12-30 13:51:39 +0100

    added b64 upload

--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from core.views import FileUploadViewSet
-from core.views import FileUploadView
+from core.views import FileUploadView, FileUploadB64ViewSet
 from django.conf.urls.static import static
 from django.conf import settings
 
 router = DefaultRouter()
 router.register(r'upload', FileUploadViewSet, basename='file-upload')
+router.register(r'upload-b64', FileUploadB64ViewSet,
+                basename='file-upload-b64')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- core/views.py
+++ core/views.py
@@ -2,11 +2,14 @@ from rest_framework import serializers
 from . models import File
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.parsers import MultiPartParser, FormParser, FileUploadParser
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.exceptions import ValidationError
 import logging
+import traceback
+import base64
+from django.core.files.base import ContentFile
 
 logger = logging.getLogger(__name__)
 
@@ -17,8 +20,26 @@ class FileSerializer(serializers.ModelSerializer):
         fields = ['id', 'file', 'uploaded_at']
 
 
+class Base64FileSerializer(serializers.ModelSerializer):
+    file = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = File
+        fields = ['id', 'file', 'uploaded_at']
+
+    def create(self, validated_data):
+        file_data = validated_data.pop('file')
+        format, file_str = file_data.split(
+            ';base64,') if ';base64,' in file_data else (None, file_data)
+        file_name = "uploaded_file.bin"  # Provide a default file name
+        decoded_file = ContentFile(base64.b64decode(file_str), name=file_name)
+
+        return File.objects.create(file=decoded_file)
+
+
 class FileUploadView(APIView):
     parser_classes = [MultiPartParser, FormParser]
+    # parser_classes = [FileUploadParser]
 
     def post(self, request, *args, **kwargs):
 
@@ -27,6 +48,12 @@ class FileUploadView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class FileUploadB64ViewSet(ModelViewSet):
+    queryset = File.objects.all()
+    serializer_class = Base64FileSerializer
+    # parser_classes = [MultiPartParser, FormParser]
 
 
 class FileUploadViewSet(ModelViewSet):
@@ -42,11 +69,17 @@ class FileUploadViewSet(ModelViewSet):
             return response
         except ValidationError as e:
             return Response(
-                data={'unexpected error': f'{e}'},
+                data={'validation error': f'{e}'},
                 status=status.HTTP_400_BAD_REQUEST
             )
         except Exception as e:
             logger.error(f'upload failed {e}')
+            return Response(
+                data={'unexpected error': f'{e}'},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+        except:
+            traceback.print_exc()
             return Response(
                 data={'unexpected error': f'{e}'},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
